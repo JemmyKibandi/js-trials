d1f8282c13b9b7bd897d3a2c4ca660766965b4cd
JemmyKibandi <jemimakibandi378@gmail.com>
2023-08-08 16:44:13 +0300

    More commits

--- js-auth/index.js
+++ js-auth/index.js
@@ -1,38 +1,61 @@
+// STEP 1: import express/ other variables
 const express = require('express');
-const app = express();
 const mysql = require('mysql');
-const port = 3000;
-// code for opening the server
+// STEP 2: declare a variable of it's method
+const app = express();
+const PORT = 3000;
+
+// MIDDLEWARE
 app.use(express.urlencoded({ extended: true })); // For parsing form data
+
+
+//STEP 4: code to connect to the database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'js_auth'
+});
+
+// STEP 5: Show that you've connected to the database
+connection.connect((err, connection) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log("Connected to the database")
+  }
+})
 
 // Define routes
 app.get('/', (req, res) => {
   res.send('Hello, this is your Node.js server!');
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Code to GET data to the database
+app.get("/users", (req, res)=>{
+  const sql = "SELECT * FROM `users`";
+  connection.query(sql,(err, results) =>{
+    if (err) {
+      res.status(500).send('Internal Server Error', err);
+    } else {
+      res.send(results);
+    }
+  }
+)})
+// Code to insert data to the database
+app.post('/register', (req, res) => {
+  const { fname, phone, email, pword, actpword } = req.body;
+  
+  const sql = "INSERT INTO `users`(`fname`, `phone`, `email`, `pword`, `actpword`) VALUES (?, ?, ?, ?, ?)";
+  connection.query(sql, [fname, phone, email, pword, actpword], (err, result) => {
+    if (err) {
+      console.error('Error inserting data:', err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.send('User added successfully');
+    }
+  });
 });
-//code to connect to the database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'js_auth'
-  });
-  app.post('/submit', (req, res) => {
-    const { name, email } = req.body;
-  
-    const sql = 'INSERT INTO users (name, email) VALUES (?, ?)';
-    connection.query(sql, [name, email], (err, result) => {
-      if (err) {
-        console.error('Error inserting data:', err);
-        res.status(500).send('Internal Server Error');
-      } else {
-        console.log('Data inserted:', result);
-        res.send('Form submitted successfully');
-      }
-    });
-  });
-  
+
+// STEP 3: listen for the port
+app.listen(PORT, () => console.log(`Server is running at port ${PORT}`))
--- js-auth/test.js
+++ js-auth/test.js
@@ -6,10 +6,26 @@ const connection = mysql.createConnection({
     database: 'js_auth'
   });
 
-  connection.connect((err) => {
+   // Code to insert data to the database
+app.post('/submit', (req, res) => {
+  const { name, phone, email, pword, actpword } = req.body;
+   // Dummy values for testing
+   const fName = 'John Doe';
+   const Phone = '1234567890';
+   const Email = 'john@example.com';
+   const Pword = 'password123';
+   const actPword = 'password123';
+ 
+
+  const sql = "INSERT INTO `users`(`fname`, `phone`, `email`, `pword`, `actpword`) VALUES (?, ?, ?, ?, ?)";
+  connection.query(sql, [fName, Phone, Email, Pword, actPword], (err, result) => {
     if (err) {
-      console.error('Error connecting to the database:', err);
+      console.error('Error inserting data:', err);
+      res.status(500).send('Internal Server Error');
     } else {
-      console.log('Connected to the database');
+      console.log('Data inserted:', result);
+      res.send('Form submitted successfully');
     }
   });
+});
+  
