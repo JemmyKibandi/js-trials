48847de91455782f17aa9e17410667c88d41612c
JemmyKibandi <jemimakibandi378@gmail.com>
2023-08-24 16:05:25 +0300

    Updatesss

--- js-auth/backend/index.js
+++ js-auth/backend/index.js
@@ -99,16 +99,16 @@ app.post('/login', async (req, res) => {
 });
 
 //ROUTE TO CARDS
-  // const filePath = path.join(__dirname, 'drag.html');
-  // fs.readFile(filePath, 'utf-8', (err, content) => {
-  //   if (err) {
-  //     res.writeHead(500);
-  //     res.end('Internal Server Error');
-  //   } else {
-  //     res.writeHead(200, { 'Content-Type': 'text/html' });
-  //     res.end(content);
-  //   }
-  // });
+  const filePath = path.join(__dirname, 'drag.html');
+  fs.readFile(filePath, 'utf-8', (err, content) => {
+    if (err) {
+      res.writeHead(500);
+      res.end('Internal Server Error');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content);
+    }
+  });
 
 // STEP LAST: listen for the port
 app.listen(PORT, () => console.log(`Server is running at port ${PORT}`))
